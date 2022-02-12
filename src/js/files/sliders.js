4b80999bffa3576640b90910a85fe7fc61a4c22d
SPashaS <SPavel.LD@gmail.com>
2022-02-12 22:04:47 +0300

    slider

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -45,11 +45,12 @@ function initSliders() {
 	bildSliders();
 
 	// Перечень слайдеров
-	if (document.querySelector('.swiper')) {
-		new Swiper('.swiper', {
+	if (document.querySelector('.innovation__slider')) {
+		new Swiper('.innovation__slider', {
 			// Подключаем модули слайдера
 			// для конкретного случая
 			//modules: [Navigation, Pagination],
+			modules: [Navigation],
 			/*
 			effect: 'fade',
 			autoplay: {
@@ -59,8 +60,8 @@ function initSliders() {
 			*/
 			observer: true,
 			observeParents: true,
-			slidesPerView: 1,
-			spaceBetween: 0,
+			slidesPerView: 4,
+			spaceBetween: 30,
 			autoHeight: true,
 			speed: 800,
 			//touchRatio: 0,
@@ -75,30 +76,27 @@ function initSliders() {
 			//},
 			// Arrows
 			navigation: {
-				nextEl: '.about__more .more__item_next',
-				prevEl: '.about__more .more__item_prev',
+				nextEl: '.innovation__slider-nav .innovation__slider-item_next',
+				prevEl: '.innovation__slider-nav .innovation__slider-item_prev',
 			},
-			/*
+			
 			breakpoints: {
-				320: {
+				0: {
 					slidesPerView: 1,
-					spaceBetween: 0,
-					autoHeight: true,
 				},
-				768: {
+				560: {
 					slidesPerView: 2,
-					spaceBetween: 20,
 				},
-				992: {
+				829: {
 					slidesPerView: 3,
 					spaceBetween: 20,
 				},
-				1268: {
+				1146: {
 					slidesPerView: 4,
 					spaceBetween: 30,
 				},
 			},
-			*/
+			
 			on: {
 
 			}
