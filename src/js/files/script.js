24f51382cce09d59b306893941dcaaf9a212a2db
SPashaS <SPavel.LD@gmail.com>
2022-02-13 20:10:23 +0300

    header fixed and sprite

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,4 +3,12 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
+function fixedHeader() {
+    if (window.scrollY >= 1) {
+        document.body.classList.add('_fixed-header');
+    } else {
+        document.body.classList.remove('_fixed-header');
+    } 
+}
 
+window.addEventListener('scroll', fixedHeader);
